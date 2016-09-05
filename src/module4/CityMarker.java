528a58d5d579047c1ede47baa9467320fab44b2a
kvpratama <kvpratama@gmail.com>
2016-09-05 20:12:02 +0800

    Draw green rectangle to represent cities

--- src/module4/CityMarker.java
+++ src/module4/CityMarker.java
@@ -43,6 +43,8 @@ public class CityMarker extends SimplePointMarker {
 		pg.pushStyle();
 		
 		// TODO: Add code to draw a triangle to represent the CityMarker
+		pg.fill(0, 255, 0);
+		pg.triangle(x, y - TRI_SIZE, x - TRI_SIZE, y + TRI_SIZE, x + TRI_SIZE, y + TRI_SIZE);
 		
 		// Restore previous drawing style
 		pg.popStyle();
